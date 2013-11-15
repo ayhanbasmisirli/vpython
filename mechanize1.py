20a0dcb6d0613f085f88227b7bea22d658de962a
ayhanbasmisirli <ayhan.basmisirli@gmail.com>
2013-11-15 04:00:31 -0500

    testing

--- mechanize1.py
+++ mechanize1.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python
+
 import mechanize
 def viewPage(url):
 	browser=mechanize.Browser()
@@ -6,6 +7,6 @@ def viewPage(url):
 	source_code=page.read()
 	print source_code
 #printing whatever url
-viewPage("http://www.google.com")
-#the most basic use of Mechanize: retrieving a website’s source code
+viewPage("http://www.ryerson.ca")
+
 
